5c20b59279fd181f59ea6c76057ad5d0f4f53ec0
Mark Hamilton <marhamil@microsoft.com>
2018-03-13 20:51:39 +0000

    Squash with stages/Text featurizer commit

--- src/lib/src/test/scala/com/microsoft/ml/spark/stages/text/MultiNGramSpec.scala
+++ src/lib/src/test/scala/com/microsoft/ml/spark/stages/text/MultiNGramSpec.scala
@@ -1,8 +1,9 @@
 // Copyright (C) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in project root for information.
 
-package com.microsoft.ml.spark
+package com.microsoft.ml.spark.stages.text
 
+import com.microsoft.ml.spark.core.test.fuzzing.{TestObject, TransformerFuzzing}
 import org.apache.spark.ml.feature.Tokenizer
 import org.apache.spark.ml.util.MLReadable
 
