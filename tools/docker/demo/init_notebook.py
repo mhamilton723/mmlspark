056873bb1508dec3b79b515bc61a38db31c4881a
marhamil723 <mhamilton723@gmail.com>
2019-09-12 09:33:10 -0400

    trigger build

--- tools/docker/demo/init_notebook.py
+++ tools/docker/demo/init_notebook.py
@@ -1,12 +1,2 @@
-from pyspark.sql import SparkSession
-import os
-spark = SparkSession.builder \
-    .master("local[*]") \
-    .appName("MMLSpark Docker App") \
-    .config("spark.jars.packages", "com.microsoft.ml.spark:mmlspark_2.11:" + os.environ["MMLSPARK_VERSION"] +
-            ",org.apache.hadoop:hadoop-azure:2.7.0") \
-    .config("spark.jars.repositories", "https://mmlspark.azureedge.net/maven") \
-    .config("spark.hadoop.fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem") \
-    .getOrCreate()
-sc = spark.sparkContext
+
 
