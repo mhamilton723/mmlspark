b5dfa5da7da9b443f79f4dc30ca1763a35019e83
marhamil723 <mhamilton723@gmail.com>
2019-09-12 00:49:48 -0400

    trigger rebuild

--- tools/docker/demo/init_notebook.py
+++ tools/docker/demo/init_notebook.py
@@ -3,7 +3,8 @@ import os
 spark = SparkSession.builder \
     .master("local[*]") \
     .appName("MMLSpark Docker App") \
-    .config("spark.jars.packages", "com.microsoft.ml.spark:mmlspark_2.11:" + os.environ["MMLSPARK_VERSION"]) \
+    .config("spark.jars.packages", "com.microsoft.ml.spark:mmlspark_2.11:" + os.environ["MMLSPARK_VERSION"] +
+            ",org.apache.hadoop:hadoop-azure:2.7.0") \
     .config("spark.jars.repositories", "https://mmlspark.azureedge.net/maven") \
     .getOrCreate()
 sc = spark.sparkContext
